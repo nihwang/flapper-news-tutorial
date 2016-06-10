6e314e882d8a90ea75a4ef1b6bacf32521429038
Nina Hwang <nihwang12@gmail.com>
2016-06-10 00:21:38 -0700

    Set up posts

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
-  		$scope.test = 'Hello world!';
-  	}
+		$scope.test = 'Hello world!';
+		$scope.posts = [
+			'Post 1',
+			'Post 2',
+			'Post 3',
+			'Post 4',
+			'Post 5'
+		]
+	}
 ]);
