49675f2948fa539fb0c18c06f33f664cf8c862ff
Nina Hwang <nihwang12@gmail.com>
2016-06-10 00:24:44 -0700

    deleted original test

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 	'$scope',
 	function($scope){
-		$scope.test = 'Hello world!';
 		$scope.posts = [
 			{title: 'Post 1', upvotes: 5},
 			{title: 'Post 2', upvotes: 2},
